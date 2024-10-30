76a08d356582189abd8fa14a9c91f269378c8645
Oleg Falko <falkooleg@gmail.com>
2024-10-30 14:49:01 +0300

    Установил плагин Neotree - hot-key -> <leader>e

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -7,12 +7,15 @@ vim.keymap.set("i", "jj", "<ESC>")
 -- Buffes
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 
+-- Neotree
+vim.keymap.set("n", "<leader>e", ":Neotree left toggle reveal<CR>")
+
 -- Navigation
 vim.keymap.set("n", "c-k", ":wincmd k<CR>")
 vim.keymap.set("n", "c-j", ":wincmd j<CR>")
 vim.keymap.set("n", "c-h", ":wincmd h<CR>")
 vim.keymap.set("n", "c-l", ":wincmd l<CR>")
-vim.keymap.set("n", "<leader>e", ":e .<CR>")
+
 
 -- Splits
 vim.keymap.set("n", "|", ":vsplit<CR>")
